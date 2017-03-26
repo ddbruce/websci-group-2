a3fd86e8dc75186c762ebf1c424971c9a8959c71
dawneraq <dawneraq@gmail.com>
2017-03-26 14:22:29 -0400

    Add HomeComponent and basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
 
+import { MenuService } from './services/menu.service';
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Welcome to {{name}}</h1>`,
+  template: `
+    <h1>{{name}}</h1>
+    <nav>
+      <a routerLink="/home" routerLinkActive="active">Home</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent  { name = 'Menuoso'; }
+export class AppComponent  {
+  name = 'Menuoso';
+
+  constructor(private menuService: MenuService) { }
+}
