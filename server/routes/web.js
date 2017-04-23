11a6a7eee81008336a64f9b2c9599d1b5974db67
dawneraq <dawneraq@gmail.com>
2017-04-23 18:16:58 -0400

    Add 404 page

--- server/routes/web.js
+++ server/routes/web.js
@@ -7,6 +7,25 @@ var absPath = path.join(__dirname, "../../src");
 //route to handle home page
 router.get("/", function(req, res, next) {
     res.sendFile(absPath + "/index.html");
-});
+}).use((req, res, next) => {
+  // Catch 404s
+  res.status = 404;
+
+  // respond with html page
+  if (req.accepts('html')) {
+      res.sendFile(absPath + "/404.html");
+      return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+      res.send({
+          error: 'Not found'
+      });
+      return;
+  }
+
+  res.type('txt').send('Not found');
+});;
 
 module.exports = router;
